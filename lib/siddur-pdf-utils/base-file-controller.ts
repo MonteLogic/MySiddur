a226c84f8db0ca35dbf9027d5a0caa71c8436a08
MonteLogic <dchavours@gmail.com>
2025-08-17 13:28:44 -0500

    comments

--- lib/siddur-pdf-utils/base-file-controller.ts
+++ lib/siddur-pdf-utils/base-file-controller.ts
@@ -18,4 +18,16 @@
 
 // We also need to have stuff in there to add onto the file. 
 
+// 
+
+
+
+// Attempt 2:
+
+// Get the necessary files and bring them in and make that work good and then get do validation after.
+
+// Build the machine and then try to validate it. 
+
+
+
 
--- prayer/prayer-validation/prayer.d.ts
+++ prayer/prayer-validation/prayer.d.ts
@@ -1,30 +1,32 @@
 // Describes the structure inside mem-prayers.json
 export interface DetailedPrayer {
-  "prayer-title": string;
-  "Introduction": string;
-  "Instruction": string;
-  "prayer-id": string; // This is the field we will check
-  "Word Mappings": {
+  'prayer-title': string;
+  Introduction: string;
+  Instruction: string;
+  'prayer-id': string; // This is the field we will check
+  'Word Mappings': {
     [key: string]: {
       hebrew: string;
       english: string;
-      "detailed-array": Array<[number, number | number[]]>;
+      'detailed-array': Array<[number, number | number[]]>;
     };
   };
 }
 
 // Describes the structure inside 0-waking-prayers.json
 export interface IndexPrayer {
-  "Introduction": string;
-  "Instruction": string;
-  "prayer-id": string; // This is the ID we expect
-  "file": string;
-  "hebrew": string;
-  "english": string;
+  Introduction: string;
+  Instruction: string;
+  'prayer-id': string; // This is the ID we expect
+  file: string;
+  hebrew: string;
+  english: string;
 }
 
-// Top-level types for the entire JSON files
-export type DetailedData = Record<string, DetailedPrayer>;
+export type DetailedData = Record<string, DetailedPrayer> & {
+  $schema?: string;
+};
+
 export type IndexData = {
-  "Waking Prayers": Record<string, IndexPrayer>;
+  'Waking Prayers': Record<string, IndexPrayer>;
 };
