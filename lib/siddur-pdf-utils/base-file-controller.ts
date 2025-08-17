dbc4f788929217f69bd28540d24039ef613e4098
MonteLogic <dchavours@gmail.com>
2025-08-17 11:22:14 -0500

    Added CUEj

---
+++ lib/siddur-pdf-utils/base-file-controller.ts
@@ -0,0 +1,21 @@
+// Get list of needed JSON objects and check them. 
+
+
+
+// Waking Prayers
+// // Get list of always in Waking Prayers from JSON files  
+// // // Take this list and find the word mapped prayers. 
+
+// Shacharis
+
+
+// Mincha 
+
+
+// Maariv 
+
+
+
+// We also need to have stuff in there to add onto the file. 
+
+
---
+++ prayer/description.md
@@ -0,0 +1,8 @@
+
+
+
+Have information about which waking prayers are most used.
+
+This will be a smaller file 
+
+
